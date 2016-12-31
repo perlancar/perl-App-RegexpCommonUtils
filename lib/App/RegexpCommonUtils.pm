64a058c4295f375da94dbaf0eebc6c5973309358
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-31 10:33:23 +0700

    Initial commit

---
+++ lib/App/RegexpCommonUtils.pm
@@ -0,0 +1,19 @@
+package App::RegexpCommonUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: CLI utilities related to Regexp::Common
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities related to L<Regexp::Common>:
+
+#INSERT_EXECS_LIST
